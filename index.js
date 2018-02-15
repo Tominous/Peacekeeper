2ab32d9189c1bdddd2dbd7a2b4ea017b19fe8a84
AEJester <aftereffectsjester@gmail.com>
2018-02-14 20:43:43 -0500

    Whoops forgot these in the first commit.

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log(client.user+" is ready!");
+});
+client.on("message", () => {
+
+    
+});
+client.login("NDEzNTA3NzQzMDE4OTA5NzE2.DWZ0tA.SrmYRFkLio5xj8PdmduHUW0wQss");
