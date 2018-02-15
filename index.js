f617c952a75c10bfb57b373b7072818fc221b7f5
AEJester <aftereffectsjester@gmail.com>
2018-02-14 20:53:05 -0500

    MySQL support in bot and dashboard.

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const mysql = require("mysql");
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'titanicRyan',
+    database : 'Peacekeeper.peacekeeper'
+});
+connection.connect();
+
+
+
 client.on("ready", () => {
     console.log(client.user+" is ready!");
 });
 client.on("message", () => {
 
-    
+
 });
 client.login("NDEzNTA3NzQzMDE4OTA5NzE2.DWZ0tA.SrmYRFkLio5xj8PdmduHUW0wQss");
